0e0d430df7af8b9ffea01a44c40a1961a6e2493b
Leonardo <leonardo.sudario06@gmail.com>
2026-04-29 10:53:35 -0300

    GeoApp

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -1,22 +1,22 @@
-import * as Location from "expo-location"; // Módulo de localizaçõ do Expo
-import { useEffect, useState } from "react"; // Os Hooks de Efeitos Colaterais e Estado/Ciclo de Vida do React
+import * as Location from "expo-location"; 
+import { useEffect, useState } from "react"; 
 
 export default function useLocation() {
   const [coords, setCoords] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
 
   useEffect(() => {
-    // Efeito colateral para obter a localização
+  
     (async () => {
       try {
-        let { status } = await Location.requestForegroundPermissionsAsync(); // Solicita permissão para acessar a localização
+        let { status } = await Location.requestForegroundPermissionsAsync(); 
         if (status !== "granted") {
           setErrorMsg("Permissão negada para acessar a localização");
           return;
         }
 
         let location = await Location.getCurrentPositionAsync({
-          accuracy: Location.Accuracy.High, // Define a acurácia como alta
+          accuracy: Location.Accuracy.High, 
         });
         setCoords({
           latitude: location.coords.latitude,
@@ -25,7 +25,7 @@ export default function useLocation() {
       } catch (error) {
         setErrorMsg("Erro ao obter a localização");
       }
-    })(); // Função auto-invoca para obter a localização
-  }, []); // O array vazio [] significa que o efeito só será executado um vez (semelhante ao componentDidMount)
+    })(); 
+  }, []); 
   return { coords, errorMsg };
 }
